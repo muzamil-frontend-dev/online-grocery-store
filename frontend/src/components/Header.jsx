ecc3f5bba96d2e813e6a78640916f3e871ae10d2
Muzamil Hussain <hmuzamil567@gmail.com>
2024-08-14 13:59:43 +0500

    header menu styling update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faCartShopping,
+  faHeart,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
     <header>
       <Container fluid>
-        <Row className="justify-content-end align-items-center">
-          <Col md={6}>
+        <Row className="align-items-center">
+          <Col className="d-md-none">
+            <div className="menu-opt">
+              <FontAwesomeIcon icon={faBars} />
+            </div>
+          </Col>
+          <Col>
             <img src="./images/logo.png" alt="App Logo" />
           </Col>
-          <Col className="text-end">
-            <div className="header-opt">
-              <FontAwesomeIcon icon={faUser} />
+          <Col>
+            <div className="d-flex justify-content-end">
+              <div className="menu-opt d-none d-md-flex">
+                <FontAwesomeIcon icon={faHeart} />
+              </div>
+              <div className="menu-opt d-none d-md-flex">
+                <FontAwesomeIcon icon={faUser} />
+              </div>
+              <div className="menu-opt">
+                <FontAwesomeIcon icon={faCartShopping} />
+              </div>
             </div>
           </Col>
         </Row>
