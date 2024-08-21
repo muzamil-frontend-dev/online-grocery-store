eab428caa2aa7a46ce9dcea36bf4e6a47fafc3ef
Muzamil Hussain <hmuzamil567@gmail.com>
2024-08-21 23:04:50 +0500

    connecting database in our project

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log(
+      `✅✅✅Application is connected with host: ${connect.connection.host}`
+    );
+  } catch (error) {
+    console.log(`❌❌❌Error connecting database : ${error.message} `.bgRed);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
